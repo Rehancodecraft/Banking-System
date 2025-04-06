da937a6bd43786a7eb4d762d51cfb19b6f6ffea9
rehancodecraft <rehan.codecraft@gmail.com>
2025-04-06 20:55:21 +0500

    Modified

--- Bank/Main.java
+++ Bank/Main.java
@@ -1,7 +1,7 @@
 package Bank;
 public class Main{
 	public static void main(String[] args){
-		//creating object
+		//creating object of class BankAccount
 		BankAccount acct1 = new BankAccount();
 		acct1.run_machine();
 
